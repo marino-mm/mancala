4f95266509c143623c0507963cbe9ed7cb712526
Marino <marinovic.matej21@gmail.com>
2026-05-18 17:32:16 +0200

    Testing new way of passing key event to consumer

--- src/bin/test_window.rs
+++ src/bin/test_window.rs
@@ -1,12 +1,12 @@
 use crossterm::cursor::MoveTo;
-use crossterm::event::{Event, KeyEvent, KeyModifiers, poll, read};
+use crossterm::event::{poll, read, Event, KeyEvent, KeyModifiers};
 use crossterm::execute;
 use crossterm::style::{Print, PrintStyledContent, StyledContent};
 use crossterm::terminal::{Clear, ClearType};
 use mancala::app::App;
 use mancala::theme::Theme;
 use std::cell::RefCell;
-use std::io::{Write, stdout};
+use std::io::{stdout, Write};
 use std::rc::Rc;
 use std::time::{Duration, Instant};
 
@@ -14,21 +14,23 @@ fn main() {
 	let mut app = App::new();
 	let mut stdout = stdout();
 
-	// let mut input_field = InputTextField::new("".into(), 25, false, app.theme.clone());
-	// let mut input_field: Rc<RefCell<Box<dyn HandleKeypress>>> =
-	//     Rc::new(
-	//         RefCell::new(
-	//             Box::new(
-	//                 InputTextField::new("".into(), 25, false, app.theme.clone())
-	//     )));
-	let mut input_field: Rc<RefCell<Box<dyn HandleKeypress>>> = Rc::new(RefCell::new(Box::new(InputNumberField::new(
+	let mut string_input_field: Rc<RefCell<Box<dyn HandleKeypress>>> =
+	    Rc::new(
+	        RefCell::new(
+	            Box::new(
+	                InputTextField::new("".into(), 25, false, app.theme.clone())
+	    )));
+	let mut number_input_field: Rc<RefCell<Box<dyn HandleKeypress>>> = Rc::new(RefCell::new(Box::new(InputNumberField::new(
 		0,
 		10,
 		false,
 		app.theme.clone(),
 	))));
-	let mut selected_element_stack: Vec<Rc<RefCell<Box<dyn HandleKeypress>>>> = Vec::with_capacity(10);
-	selected_element_stack.push(input_field.clone());
+	let mut selected_element:Option<Rc<RefCell<Box<dyn HandleKeypress>>>> = None;
+	let mut selectable_elements_list = Vec::new();
+
+	selectable_elements_list.push(string_input_field.clone());
+	selectable_elements_list.push(number_input_field.clone());
 
 	let target_fps = 60;
 	let target_duration_micros = 1000000 / target_fps;
@@ -45,26 +47,52 @@ fn main() {
 							app.running = false;
 							break;
 						}
-						selected_element_stack
-							.last()
-							.unwrap()
-							.borrow_mut()
-							.handle_keypress(key_event);
-						// input_field.handle_keypress(key_event);
+						if key_event.code.is_left() {
+							selected_element = selectable_elements_list.first().cloned();
+						}
+						if key_event.code.is_right(){
+							selected_element = selectable_elements_list.last().cloned();
+						}
+						match selected_element {
+							Some(ref e) => {
+								e.borrow_mut().handle_keypress(key_event);
+							}
+							None => {}
+						}
 					}
 				}
 				_ => {}
 			}
 		}
-		execute!(
-			stdout,
-			MoveTo(0, 0),
-			Clear(ClearType::CurrentLine),
-			PrintStyledContent(input_field.borrow().get_styled_content()),
-		)
-		.expect("Error putting content into queue");
+		for (row, elem) in selectable_elements_list.iter().enumerate(){
+			let (x, y) = test_fn();
+			execute!(stdout,
+				MoveTo(0, row as u16),
+				Clear(ClearType::CurrentLine),
+				PrintStyledContent(elem.borrow().get_styled_content()),
+				x, y
+			).unwrap();
+		}
+		// execute!(
+		// 	stdout,
+		// 	MoveTo(0, 0),
+		// 	Clear(ClearType::CurrentLine),
+		// 	PrintStyledContent(input_field.borrow().get_styled_content()),
+		// )
+		// .expect("Error putting content into queue");
 		sleep_time = target_duration_micros - start_time.elapsed().as_micros() as u64;
 	}
+}
+
+fn test_fn() -> (Print<String>, PrintStyledContent<String>) {
+	let test_string = String::from("Hello, world!");
+	let styled_content = StyledContent::new(
+		Theme::default().get_content_style(),
+		test_string.clone()
+	);
+	let print_empty = Print(" ".repeat(test_string.chars().count()));
+	let print_styled_content = PrintStyledContent(styled_content);
+	(print_empty, print_styled_content)
 }
 
 trait HandleKeypress {
@@ -74,7 +102,7 @@ trait HandleKeypress {
 
 struct InputTextField {
 	text: String,
-	max_width: usize,
+	pub max_width: usize,
 	is_highlighted: bool,
 	theme: Theme,
 }
@@ -93,15 +121,6 @@ impl HandleKeypress for InputTextField {
 		if key.code.is_backspace() | key.code.is_delete() {
 			self.text.pop();
 		}
-		execute!(
-			stdout(),
-			MoveTo(0, 1),
-			// Clear(ClearType::CurrentLine),
-			Print(String::from(" ").repeat(self.max_width)),
-			MoveTo(0, 1),
-			Print(format!("{:?}", key)),
-		)
-		.expect("Error putting content into queue");
 	}
 
 	fn get_styled_content(&self) -> StyledContent<String> {
@@ -205,15 +224,6 @@ impl HandleKeypress for InputNumberField {
 		if key.code.is_backspace() | key.code.is_delete() {
 			self.content_number /= 10;
 		}
-		execute!(
-			stdout(),
-			MoveTo(0, 2),
-			// Clear(ClearType::CurrentLine),
-			Print(String::from(" ").repeat(self.max_width)),
-			MoveTo(0, 2),
-			Print(format!("{:?}", key)),
-		)
-		.expect("Error putting content into queue");
 	}
 
 	fn get_styled_content(&self) -> StyledContent<String> {
